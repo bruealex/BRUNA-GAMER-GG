c879c1f2cc08e32a713a3c2b53d9dae061965834
bruna <87388357+bruealex@users.noreply.github.com>
2026-07-13 08:15:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-<script src="script.js"></script>
-
 async function carregarLoja() {
     try {
         const resposta = await fetch("https://fortnite-api.com/v2/shop/br");
-
         const dados = await resposta.json();
 
-        console.log(dados);
+        const loja = document.getElementById("loja");
+        loja.innerHTML = "";
+
+        const itens = dados.data.entries.slice(0, 8);
+
+        itens.forEach(item => {
+            const nome = item.items[0].name;
+            const imagem = item.items[0].images.icon;
+            const preco = item.finalPrice;
+
+            loja.innerHTML += `
+                <div class="item-loja">
+                    <img src="${imagem}" width="120">
+                    <h4>${nome}</h4>
+                    <p>${preco} V-Bucks</p>
+                </div>
+            `;
+        });
 
     } catch (erro) {
-        console.log("Erro ao carregar a loja:", erro);
+        console.error(erro);
     }
 }
 
